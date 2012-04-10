166946af19586584b3796ba5880557bbace10392
Brian Cain <brianccain@gmail.com>
2012-04-09 22:35:26 -0500

    Pushing changes for now before major overhaul

---
+++ Pickle/easy_client.py
@@ -0,0 +1,72 @@
+try:
+  import cPickle as pickle
+except:
+  import pickle
+
+import pprint
+from StringIO import StringIO
+
+import socket
+import sys
+
+def get_constants(prefix):
+    """Create a dictionary mapping socket module constants to their names."""
+    return dict( (getattr(socket, n), n)
+                 for n in dir(socket)
+                 if n.startswith(prefix)
+                 )
+
+families = get_constants('AF_')
+types = get_constants('SOCK_')
+protocols = get_constants('IPPROTO_')
+
+# Create a TCP/IP socket
+sock = socket.create_connection(('localhost', 10000))
+
+print >>sys.stderr, 'Family  :', families[sock.family]
+print >>sys.stderr, 'Type    :', types[sock.type]
+print >>sys.stderr, 'Protocol:', protocols[sock.proto]
+print >>sys.stderr
+
+class SimpleObject(object):
+
+  def __init__(self, name):
+    self.name = name
+    l = list(name)
+    l.reverse()
+    self.name_backwards = ''.join(l)
+    return
+
+data = []
+data.append(SimpleObject('pickle'))
+data.append(SimpleObject('cPickle'))
+data.append(SimpleObject('last'))
+
+out_s = StringIO()
+
+try:
+    buffer_size = 256
+    
+    # Send data
+
+    for o in data:
+      print "WRITING: %s (%s)" % (o.name, o.name_backwards)
+      f = sock.makefile('wb', buffer_size)
+      pickle.dump(o, f, pickle.HIGHEST_PROTOCOL)
+      f.close()
+
+    message = f
+    print >>sys.stderr, 'sending "%s"' % message
+    sock.sendall(message)
+
+    amount_received = 0
+    amount_expected = len(message)
+    
+    while amount_received < amount_expected:
+        data = sock.recv(16)
+        amount_received += len(data)
+        print >>sys.stderr, 'received "%s"' % data
+
+finally:
+    print >>sys.stderr, 'closing socket'
+    sock.close()
